5c7a6546ce5bd6e7867f0a0ddd3ff16444461b1a
Paul Schreiber <github@paulschreiber.com>
2017-10-13 15:57:33 -0400

    Move constants to top, don't repeat headers

--- lambda_function.py
+++ lambda_function.py
@@ -3,49 +3,53 @@ import json
 import urllib2
 import datetime
 
+USER_AGENT = "Jane Doe Slack/Basecamp"
+AUTH_HEADER = "XXXXXXXXX"
+
+BASECAMP_ID = 1111111
+BASECAMP_PROJECT_ID = 111111
+SLACK_WEBHOOK_ID = "1111/2222/3333"
+SLACK_TESTING_CHANNEL = "#channel-name"
+SLACK_PRODUCTION_CHANNEL = "#channel-name"
+
+HTTP_HEADERS = {
+    'user-agent': USER_AGENT,
+    'content-type': "application/json",
+    'authorization': AUTH_HEADER, # use postman to generate this token
+    'cache-control': "no-cache",
+}
+
+def basecamp_url( itemType ):
+    return "/%s/api/v1/projects/%s/%s.json?" % (BASECAMP_ID, BASECAMP_PROJECT_ID, itemType)
+
+
 def whose_out():
     import httplib
 
     conn = httplib.HTTPSConnection("basecamp.com")
-    
-    headers = {
-        'user-agent': "MyApp YourEmailHere",
-        'content-type': "application/json",
-        'authorization': "Basic YourAuthHere", # use postman to generate this token
-        'cache-control': "no-cache",
-        }
-    
-    conn.request("GET", "/YourBaseCampIDHere/api/v1/projects/YourBaseCampProjectIdHere/calendar_events.json?", headers=headers)
-    
+    conn.request("GET", basecamp_url('calendar_events'), headers=HTTP_HEADERS)
+
     res = conn.getresponse()
     data = res.read()
     message = '*Who is out today %s (Calendar)*' % datetime.datetime.now().strftime("%Y-%m-%d")
-    
+
     events = json.loads((data.decode("utf-8")))
     for event in events:
         if event["starts_at"][0:10] <= datetime.datetime.now().strftime("%Y-%m-%d") <= event["ends_at"][0:10]:
             message += "\n==============\n*Person:* %s\n*Reason:* %s\n*Description*: %s\n*URL:* %s\n" % (event["creator"]["name"], event["summary"], event["description"], event["app_url"])
-        
+
     return message
 
 def whose_out_post():
     import httplib
 
     conn = httplib.HTTPSConnection("basecamp.com")
-    
-    headers = {
-        'user-agent': "MyApp YourEmailHere",
-        'content-type': "application/json",
-        'authorization': "Basic YourAuthHere", # use postman to generate this token
-        'cache-control': "no-cache",
-        }
-    
-    conn.request("GET", "/YourBaseCampIDHere/api/v1/projects/YourBaseCampProjectIdHere/events.json?", headers=headers)
-    
+    conn.request("GET", basecamp_url('events'), headers=HTTP_HEADERS)
+
     res = conn.getresponse()
     data = res.read()
     message = '\n*Who is out today %s (Basecamp Post)*' % datetime.datetime.now().strftime("%Y-%m-%d")
-    
+
     events = json.loads((data.decode("utf-8")))
     year = datetime.datetime.now().strftime("%Y")
     day = datetime.datetime.now().strftime("%d")
@@ -55,52 +59,37 @@ def whose_out_post():
             if "/" + year in event["target"]:
                 if (month + "/" + year not in event["target"] and day != month) and (month.lstrip("0") + "/" + year not in event["target"] and day.lstrip("0") != month.lstrip("0")):
                     message += "\n==============\n*Person:* %s\n*Reason:* %s\n*Description*: %s\n*URL:* %s\n" % (event["creator"]["name"], event["target"], event["excerpt"], event["html_url"])
-        
+
     return message
 
 def whose_out_tommorow():
     import httplib
 
     conn = httplib.HTTPSConnection("basecamp.com")
-    
-    headers = {
-        'user-agent': "MyApp YourEmailHere",
-        'content-type': "application/json",
-        'authorization': "Basic YourAuthHere", # use postman to generate this token
-        'cache-control': "no-cache",
-        }
-    
-    conn.request("GET", "/YourBaseCampIDHere/api/v1/projects/YourBaseCampProjectIdHere/calendar_events.json?", headers=headers)
-    
+    conn.request("GET", basecamp_url('calendar_events'), headers=HTTP_HEADERS)
+
     res = conn.getresponse()
     data = res.read()
     message = '*Who is out tommorow %s (Calendar)*' % (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-    
+
     events = json.loads((data.decode("utf-8")))
     for event in events:
         if event["starts_at"][0:10] <= (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d") <= event["ends_at"][0:10]:
             message += "\n==============\n*Person:* %s\n*Reason:* %s\n*Description*: %s\n*URL:* %s\n" % (event["creator"]["name"], event["summary"], event["description"], event["app_url"])
-        
+
     return message
 
 def whose_out_tommorow_post():
     import httplib
 
     conn = httplib.HTTPSConnection("basecamp.com")
-    
-    headers = {
-            'user-agent': "MyApp YourEmailHere",
-            'content-type': "application/json",
-            'authorization': "Basic YourAuthHere", # use postman to generate this token
-            'cache-control': "no-cache",
-            }
-    
-    conn.request("GET", "/YourBaseCampIDHere/api/v1/projects/YourBaseCampProjectIdHere/events.json?", headers=headers)
-    
+
+    conn.request("GET", basecamp_url('events'), headers=HTTP_HEADERS)
+
     res = conn.getresponse()
     data = res.read()
     message = '\n*Who is out tommorow %s (Basecamp Post)*' % (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-    
+
     events = json.loads((data.decode("utf-8")))
     year = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y")
     day = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%d")
@@ -110,22 +99,22 @@ def whose_out_tommorow_post():
             if "/" + year in event["target"]:
                 if (month + "/" + year not in event["target"] and day != month) and (month.lstrip("0") + "/" + year not in event["target"] and day.lstrip("0") != month.lstrip("0")):
                     message += "\n==============\n*Person:* %s\n*Reason:* %s\n*Description*: %s\n*URL:* %s\n" % (event["creator"]["name"], event["target"], event["excerpt"], event["html_url"])
-        
+
     return message
 
 
 def post_to_slack(report_content):
     testing=False
-    url = ("https://hooks.slack.com/services/" + "YourSlackHookURLHere")
+    url = ("https://hooks.slack.com/services/" + SLACK_WEBHOOK_ID)
     # Send to private channel while testing
     if(testing):
         values={'text':report_content,
-               'channel':"@YourNameHere"}
+               'channel':SLACK_TESTING_CHANNEL}
         values=json.dumps(values)
 
     else:
         values={'text':report_content,
-               'channel':"#YourChannelHere"}
+               'channel':SLACK_PRODUCTION_CHANNEL}
         values=json.dumps(values)
     req = urllib2.Request(url, values)
     response = urllib2.urlopen(req)
@@ -134,7 +123,7 @@ def lambda_handler(event,context):
     content_str = ''
     content_str += whose_out()
     content_str += whose_out_post()
-    
+
     content_str += whose_out_tommorow()
     content_str += whose_out_tommorow_post()
     #print content_str
@@ -145,7 +134,7 @@ if __name__ == '__main__':
 
     content_str += whose_out()
     content_str += whose_out_post()
-    
+
     content_str += whose_out_tommorow()
     content_str += whose_out_tommorow_post()
     #print content_str
